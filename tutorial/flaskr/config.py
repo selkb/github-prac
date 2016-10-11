06074d009104bd02ed0b3de4d037925d41591123
selkb <selkb@users.noreply.github.com>
2016-10-11 15:57:05 +0900

    Add files via upload

---
+++ tutorial/flaskr/config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://flaskr:FlaskApp5.7@192.168.56.103/entries?charset=utf8'
+SECRET_KEY = 'secret key'
---
+++ tutorial/manage.py
@@ -0,0 +1,2 @@
+from flaskr import app
+app.run(host='192.168.56.103', port=5000, debug=True)
